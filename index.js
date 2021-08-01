e18ee3462eeecb05eb402426d00dc6e2eb5d0dc6
Emondasgithub1 <emon.1000das@gmail.com>
2021-08-01 14:27:08 +0600

    first

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://doctorPortal:doctorPortal@cluster0.9pksi.mongodb.net/doctorPortalUser?retryWrites=true&w=majority";
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("doctorPortalUser").collection("user");
+  const collectionAppoint = client.db("doctorPortalUser").collection("appoint");
+    app.post('/addUser',(req,res) => {
+      collection.insertOne(req.body)
+    })
+    app.get('/get',(req,res) => {
+      collection.find({})
+      .toArray((err,documents) => {
+        res.send(documents);
+      })
+    })
+    app.post('/appoint',(req,res) => {
+      collectionAppoint.insertOne(req.body)
+    })
+    app.get('/getAppoint',(req,res) => {
+      collectionAppoint.find({})
+      .toArray((err,documents) => {
+        res.send(documents)
+      })
+    })
+});
+
+app.listen(4000)
